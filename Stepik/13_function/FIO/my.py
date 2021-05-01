f19ccefb399b3285ed921c559ec0b298734b7c10
Surgeon76 <surgeon76@outlook.com>
2021-05-01 22:45:18 +0300

    end

--- Stepik/13_function/FIO/my.py
+++ Stepik/13_function/FIO/my.py
@@ -1,6 +1,6 @@
 # объявление функции
 def print_fio(name, surname, patronymic):
-    print(name[0].upper() + surname[0].upper() + patronymic[0].upper())
+    print(surname[0].upper() + name[0].upper() + patronymic[0].upper())
 
 # считываем данные
 name, surname, patronymic = input(), input(), input()
