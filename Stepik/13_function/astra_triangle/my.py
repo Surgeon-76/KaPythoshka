5bbfa2784eb36da5d24b3f0d36df580429fb1b3d
Surgeon76 <surgeon76@outlook.com>
2021-05-01 22:31:49 +0300

    final

--- Stepik/13_function/astra_triangle/my.py
+++ Stepik/13_function/astra_triangle/my.py
@@ -1,7 +1,7 @@
 # объявление функции
 def draw_triangle(fill, base):
     for i in range(base):
-        print(*[fill for n in range((base // 2 + 1) - abs(base // 2 - i))])
+        print(*[fill for n in range((base // 2 + 1) - abs(base // 2 - i))], sep = '')
 
 # считываем данные
 fill = input()
