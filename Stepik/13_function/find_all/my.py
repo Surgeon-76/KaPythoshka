1dfd2acf3775ec2521397459fcb0430a7f729899
Surgeon76 <Surgeon76@yandex.ru>
2021-05-15 18:51:54 +0300

    final

--- Stepik/13_function/find_all/my.py
+++ Stepik/13_function/find_all/my.py
@@ -1,15 +1,10 @@
 # объявление функции
 def find_all(target, symbol):
-    m = list(target)
-
-    print(m)
-
-    #return [i for i in range(len(target)) if symbol == s[i]]  ????????
-
+    return [i for i in range(len(target)) if symbol == s[i]]
 # считываем данные
 s = input()
 char = input()
 
 # вызываем функцию
 find_all(s, char)
-#print(find_all(s, char))
+print(find_all(s, char))
