b8fc6b0a0a2bdff450f0ec36cddc77972c99b09f
Surgeon76 <surgeon76@outlook.com>
2021-05-15 06:15:16 +0300

    1

---
+++ Stepik/13_function/find_all/my.py
@@ -0,0 +1,14 @@
+# объявление функции
+def find_all(target, symbol):
+    m = list(target)
+    print(m)
+
+    #return [i for i in range...]
+
+# считываем данные
+s = input()
+char = input()
+
+# вызываем функцию
+find_all(s, char)
+#print(find_all(s, char))
