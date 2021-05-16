2c9fdd7cb0a4a4e960f3dfeefc44b87f955f91ee
Surgeon76 <surgeon76@outlook.com>
2021-05-17 00:29:58 +0300

    final

--- Stepik/13_function/next_prime/my.py
+++ Stepik/13_function/next_prime/my.py
@@ -1,10 +1,13 @@
 # объявление функции
 def get_next_prime(num):
-    k = n
-    while not len([n for n in range(1, k + 1) if num % n == 0]) == 2:
-        k += 1
-    
-
+    k = num
+    flag = True
+    while flag:
+        if len([n for n in range(1, k + 1) if k % n == 0]) != 2 or k == num:
+            k += 1
+        else:
+            flag = False     
+    return k
 
 # считываем данные
 n = int(input())
