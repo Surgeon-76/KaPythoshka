01ee7a65162b3f7edb14c38287f478815c74a201
Surgeon76 <surgeon76@outlook.com>
2021-05-22 23:52:45 +0300

    final

--- Stepik/13_function/exactly_one/my.py
+++ Stepik/13_function/exactly_one/my.py
@@ -1,6 +1,16 @@
 # объявление функции
 def is_one_away(word1, word2):
-    
+    if len(word1) == len(word2):
+        flag = True
+        n = 0
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                n += 1
+        if n != 1:
+            flag = False
+    else:
+        flag = False
+    return flag
 
 # считываем данные
 txt1 = input()
