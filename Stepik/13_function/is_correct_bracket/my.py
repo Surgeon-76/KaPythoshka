f46eecf79498585ef2412c41baa1c87f6f0cd24b
Surgeon76 <Surgeon76@yandex.ru>
2021-05-28 22:42:30 +0300

    finish

--- Stepik/13_function/is_correct_bracket/my.py
+++ Stepik/13_function/is_correct_bracket/my.py
@@ -1,9 +1,8 @@
 # объявление функции
 def is_correct_bracket(text):
-    for i in range(10):
-        n = 1 +
-        2
-        
+    while text.find('()') != -1:
+        text = text.replace('()', '')
+    return len(text) == 0
 
 # считываем данные
 txt = input()
