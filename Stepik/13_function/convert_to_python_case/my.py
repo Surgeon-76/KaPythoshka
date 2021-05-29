95211fdad2701383db9d9849d911d367769dec64
Surgeon76 <surgeon76@outlook.com>
2021-05-29 10:57:37 +0300

    finish

--- Stepik/13_function/convert_to_python_case/my.py
+++ Stepik/13_function/convert_to_python_case/my.py
@@ -1,10 +1,10 @@
 # объявление функции
 def convert_to_python_case(text):
     snake_style = ''
-    for i in text:
-        if i.isupper() and i != text[0]:
+    for i in range(len(text)):
+        if text[i].isupper() and i != 0:
             snake_style += '_'
-        snake_style += i.lower()
+        snake_style += text[i].lower()
     return snake_style
     
 # считываем данные
