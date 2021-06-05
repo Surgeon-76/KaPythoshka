b76362b37ff36c79d2ec6d4dd4a6569c24079a1a
Surgeon76 <Surgeon76@yandex.ru>
2021-06-06 01:41:39 +0300

    temp

--- Stepik/15_mini_project/cezar/my.py
+++ Stepik/15_mini_project/cezar/my.py
@@ -3,9 +3,11 @@ direction = input('Выбери направление: \n(+) - Шифрован
 lang = int(input('Выбери язык алфавита: \n0 - Русский \n1 - Английский: \n'))
 step = int(direction + input('Веди число, шаг сдвига (со сдвигом вправо): '))
 text = input('Введите текст для обработки:\n')
-
+punctuation = '!#$%&*+-=?@^_'
 for i in text:
-    if i.isalpha():
+    if  not i.isalpha():
+        for ch in i:
+            
         char = alph[lang][(alph[lang].index(i.upper()) + step) % len(alph[lang])]
         print(char if i.isupper() else char.lower(), end='')
     else:
---
+++ Stepik/15_mini_project/cezar/my_2.py
@@ -0,0 +1,10 @@
+alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#direction = input('Выбери направление: \n(+) - Шифрование \n(-) - Дешифрование:\n ')
+#lang = int(input('Выбери язык алфавита: \n0 - Русский \n1 - Английский: \n'))
+#step = int(direction + input('Веди число, шаг сдвига (со сдвигом вправо): '))
+text = input('Введите текст для обработки:\n').split()
+print(text)
+
+for i in text:
+    if i.isprintable
+    print(i, len(i))
