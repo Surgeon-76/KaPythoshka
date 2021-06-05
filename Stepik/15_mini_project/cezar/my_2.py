f345166f11177cda13055ae898c744cee4101c35
Surgeon76 <Surgeon76@yandex.ru>
2021-06-06 02:23:04 +0300

    3

--- Stepik/15_mini_project/cezar/my_2.py
+++ Stepik/15_mini_project/cezar/my_2.py
@@ -1,16 +1,13 @@
 #функция количества буквенных элементов в слове
 def length_words(words):
-    count = 0
-    for k in (words):
-        if k.isalpha():
-            count += 1
-    return count
+    return len([k for k in words if k.isalpha()])
 
 #основной цикл программы
 f_text = []
 text = input('Введите текст для обработки:\n').split()
+
 for i in text:
-    for j in len() 
+    print(i, length_words(i))
     
     
     length_words(i)
