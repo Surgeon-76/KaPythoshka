eab5f46bf22fc194824194d959517a3494258547
Surgeon76 <Surgeon76@yandex.ru>
2021-06-06 07:48:32 +0300

    finish

--- Stepik/15_mini_project/cezar/my_2.py
+++ Stepik/15_mini_project/cezar/my_2.py
@@ -4,12 +4,18 @@ def length_words(words):
 
 #основной цикл программы
 f_text = []
-text = input('Введите текст для обработки:\n').split()
+for i in input().split():
+    temp = ''
+    for j in i:
+        symb_ord = 0
+        if j.isalpha():
+            symb_ord = ord(j) + length_words(i)
+            if (ord(j) < 90 and symb_ord > 90) or (90 < ord(j) < 122 and symb_ord > 122):
+                symb_ord -= 26
+            temp += chr(symb_ord)
+        else:
+            temp += j
+    f_text.append(temp) 
+print(*f_text)
 
-for i in text:
-    print(i, length_words(i))
-    
-    
- #   length_words(i)
-    
 
