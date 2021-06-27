1c0cdf992a2f64350bd7ffdb8148a651dfe94240
Surgeon76 <surgeon76@outlook.com>
2021-06-27 18:57:04 +0300

    index search

--- Stepik_prodvin/stone_scissors_paper_lizard_Spock/my_2.py
+++ Stepik_prodvin/stone_scissors_paper_lizard_Spock/my_2.py
@@ -1,15 +1,29 @@
-variants = [['камень', 'камень'], ['ящерица', 'ножницы'], ['Спок', 'ящерица'], ['ножницы', 'бумага'], ['бумага', 'Спок']]
+variants = [['камен', 'камень'], ['ящерица', 'ножницы'], ['Спок', 'ящерица'], ['ножницы', 'бумага'], ['бумага', 'Спок']]
 winner = ['ничья', 'Руслан', 'Тимур','Руслан', 'Тимур']
 y = 0
+def index_search(data_str, array):
+    for subarray in array:
+        if data_str in subarray:
+            print(array.index(subarray), '-', subarray.index(data_str))
+
 """
 def choice_of_index(timur, ruslan, y):
     return variants.index(timur)[y] - variants.index(ruslan)[y]
 """
 timur, ruslan = input(), input()
-print(variants[variants.index(timur)][y])
+index_search(timur, variants)
+print('==')
+index_search(ruslan, variants)
+#print(variants[variants.index(timur)][y])
 
 """
 if (choice_of_index(timur, ruslan, y) != 1) or (choice_of_index(timur, ruslan, y) != 4) or (choice_of_index(timur, ruslan, y) != -4) or (choice_of_index(timur, ruslan, y) != -1):
     y = 1
 print(winner[variants[y].index(timur) - variants[y].index(ruslan)])
+"""
+"""
+array = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h']]
+for subarray in array:
+    if 'a' in subarray:
+        print(array.index(subarray), '-', subarray.index('a'))
 """
