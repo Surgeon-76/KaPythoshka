ffb14dd3c93400bc934cdc4cec24e95979c58aaf
Surgeon-76 <Surgeon76@yandex.ru>
2023-08-04 21:27:39 +0300

    9

--- Stepik_OOP/methods/heroic_9.py
+++ Stepik_OOP/methods/heroic_9.py
@@ -3,8 +3,13 @@ import sys
 # программу не менять, только добавить два метода
 # lst_in = list(map(str.strip, sys.stdin.readlines()))  # считывание списка строк из входного потока
 
-lst_in = [["1", "Сергей", "35", "120000"],
-["2," "Федор," "23", "12000"]]
+# lst_in = [["1", "Сергей", "35", "120000"],
+#           ["2", "Федор", "23", "12000"],
+#           ["3", "Иван", "13", "1200"]]
+
+lst_in = ["1 Сергей 35 120000",
+          "2 Федор 23 12000",
+          "3 Иван 13 1200"]
 
 
 class DataBase:
@@ -14,8 +19,15 @@ class DataBase:
     # здесь добавлять методы
 
     def insert(self, data):
-        lst_data = 
+        for lst in data:
+            self.lst_data.append(dict(zip(self.FIELDS, lst.split())))
 
+    def select(self, a, b):
+        b = min(b, len(self.lst_data))
+        return self.lst_data[a:b]
 
 db = DataBase()
 db.insert(lst_in)
+print(db.lst_data)
+print()
+print(db.select(2, 10))
